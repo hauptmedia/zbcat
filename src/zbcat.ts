07d748d1da4812e8dedc1d5333a9bc61cdcac689
Julian Haupt <julian@haupt.media>
2022-11-23 18:04:50 +0100

    made everything configurable

--- src/zbcat.ts
+++ src/zbcat.ts
@@ -2,16 +2,36 @@ import {Kafka} from 'kafkajs';
 import {TablePrintRecordHandler} from "./TablePrintRecordHandler";
 import {dispatchZeebeRecordToHandler, ValueType, ZeebeRecord} from "@hauptmedia/zeebe-exporter-types";
 import * as uuid from 'uuid';
+import { Command } from 'commander';
+
+const program = new Command()
+    .option('--fields <fields>')
+    .option('--sample-rate <sample_rate>')
+    .option('--from-beginning')
+    .option('--kafka-brokers <broker_list>')
+    .option('--kafka-client-id <kafka_client_id>')
+    .option('--kafka-group-id <kafka_group_id>')
+    .option('--kafka-topics <kafka_topics>');
+
+
+program.parse();
+const options = program.opts();
+
+const
+    fields = options['fields'] || process.env.ZBCAT_FIELDS || 'bpmnElementType,elementId,correlationKey,variables,decisionId,errorType,errorMessage',
+    sampleRate = options['sampleRate'] || process.env.ZBCAT_SAMPLE_RATE || 2000,
+    kafkaFromBeginning = options['fromBeginning'] || process.env.ZBCAT_KAFKA_FROM_BEGINNING || false,
+    kafkaBrokers = options['kafkaBrokers'] || process.env.ZBCAT_KAFKA_BROKERS || 'localhost:9092',
+    kafkaClientId = options['kafkaClientId'] || process.env.ZBCAT_KAFKA_CLIENT_ID || 'zbcat',
+    kafkaGroupId = options['kafkaGroupId'] || process.env.ZBCAT_KAFKA_GROUP_ID || uuid.v4(),
+    kafkaTopics = options['kafkaTopics'] || process.env.ZBCAT_KAFKA_TOPICS || 'zeebe';
 
 const kafka = new Kafka({
-    clientId: 'zbcat',
-    brokers: ['localhost:9093']
-})
-
-const fields = ['bpmnElementType', 'elementId', 'correlationKey', 'variables', 'decisionId', 'errorType', 'errorMessage'],
-    sampleRate = 2000,
-    zbRecordHandler = new TablePrintRecordHandler(fields, sampleRate),
-    consumer = kafka.consumer({groupId: uuid.v4()})
+        clientId: kafkaClientId,
+        brokers: kafkaBrokers.split(",")
+    }),
+    zbRecordHandler = new TablePrintRecordHandler(fields.split(","), parseInt(sampleRate, 10)),
+    consumer = kafka.consumer({groupId: kafkaGroupId})
 
 process.on('SIGINT', () => {
     consumer.disconnect().then(() => process.exit());
@@ -19,7 +39,10 @@ process.on('SIGINT', () => {
 
 const run = async () => {
     await consumer.connect()
-    await consumer.subscribe({topic: "zeebe"})
+    await consumer.subscribe({
+        topics: kafkaTopics.split(","),
+        fromBeginning: !!kafkaFromBeginning
+    })
 
     await consumer.run({
         eachMessage: async ({topic, partition, message, heartbeat, pause}) => {
